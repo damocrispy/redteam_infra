d232e9e20d992837e373d39c42f7b8efa7fe92f5
damocrispy <damian.christie@pm.me>
2024-09-17 11:49:13 +0100

    public_key changed to sensitive local.

--- locals.tf
+++ locals.tf
@@ -4,4 +4,6 @@ locals {
     aws_default_subnet.default_subnet_b.id,
     aws_default_subnet.default_subnet_c.id
   ]
+
+  public_key = sensitive(file("~/.ssh/redteam_main.pub"))
 }
--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "public_key" {
-  description = "Public key for access to EC2 instance."
-  type        = string
-}
