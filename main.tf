09a183d70a51dc3f40fcb17ef50ab2ead94d1d53
damocrispy <damian.christie@pm.me>
2024-09-17 12:02:18 +0100

    main updated for local.public.key

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_default_subnet" "default_subnet_c" {
 
 resource "aws_key_pair" "redteam_keys" {
   key_name   = "redteam_main"
-  public_key = var.public_key
+  public_key = local.public_key
 }
 
 resource "aws_instance" "ec2_vm" {
@@ -143,8 +143,8 @@ resource "aws_lb_target_group" "target_group" {
 
 resource "aws_lb_target_group_attachment" "target_attachment" {
   target_group_arn = aws_lb_target_group.target_group.arn
-  target_id = aws_instance.ec2_vm.id
-  port = 80
+  target_id        = aws_instance.ec2_vm.id
+  port             = 80
 }
 
 resource "aws_lb_listener" "listener" {
